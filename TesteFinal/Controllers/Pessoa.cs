c7dff6b550cda960e23caaddc933c29410def141
RodolfoSantana8 <129907500+RodolfoSantana8@users.noreply.github.com>
2024-02-07 22:17:52 -0300

    TesteFinal

---
+++ TesteFinal/Controllers/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TesteFinal.Controllers
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Nome { get; set; }
+
+        [MaxLength(14)]
+        public string Cpf { get; set; }
+        public DateOnly DataNascimento { get; set; }
+    }
+}
